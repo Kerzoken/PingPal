998fed4f9065300b13f4396e8ff4335284b221ed
Денис Мирчев <78972321+Kerzoken@users.noreply.github.com>
2024-01-20 04:23:02 +0200

    Revert "Created a db connection in app.py"
    This reverts commit a02475b985027bf688f0ac4cef6930c03ce3a281.

--- api/app.py
+++ api/app.py
@@ -1,28 +1,8 @@
-"""
-This module initializes the Flask application.
-"""
-
-import os
-from dotenv import load_dotenv
-
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-load_dotenv('../.env')
-
-MYSQL_USER = os.getenv('MYSQL_USER')
-MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
-MYSQL_HOST = os.getenv('MYSQL_HOST')
-MYSQL_PORT = os.getenv('MYSQL_PORT')
-MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
 
 app = Flask(__name__)
 
-db_uri = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
-app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-db = SQLAlchemy(app)
-
-if __name__ == '__main__':
-    app.run(debug=True)
