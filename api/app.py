1019387f4e3af3914240520ef0627703b973b2a1
Kerzoken <kerzo.gm@gmail.com>
2024-01-20 03:11:51 +0200

    Added docker compose file, scripts and adjusted project files

--- api/app.py
+++ api/app.py
@@ -1,31 +1,31 @@
 from flask import Flask
-from api.views.users import users_bp
-from api.views.messages import messages_bp
-from api.models import db
-
-from api import config
-
+from flask_migrate import Migrate
 from flask_swagger_ui import get_swaggerui_blueprint
-
-SWAGGER_URL = "/swagger"
-API_URL = "/static/swagger.json"
-
-
-swagger_ui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': 'Access API'
-    }
-)
+from .views.users import users_bp
+from .views.messages import messages_bp
+from . import config
+from .db import db
 
 app = Flask(__name__)
 app.config.from_object(config)
 
 db.init_app(app)
 
+migrate = Migrate(app, db)
+migrate.init_app(app, db)
+
 app.register_blueprint(users_bp, url_prefix='/users')
 app.register_blueprint(messages_bp, url_prefix='/messages')
+
+
+SWAGGER_URL = "/swagger"
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    "/static/swagger.json",
+    config={
+        'app_name': 'PingPal API'
+    }
+)
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
