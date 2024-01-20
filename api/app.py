025d3f4e406993bd0d3227d5a2d9d43e6b5535fc
Денис Мирчев <78972321+Kerzoken@users.noreply.github.com>
2024-01-20 04:23:02 +0200

    Revert "Implemented /messages endpoints"
    This reverts commit 1c42edbf8ea1f4a5a62b340dea375b56846744cc.

--- api/app.py
+++ api/app.py
@@ -1,23 +1,8 @@
 from flask import Flask
 from api.views.users import users_bp
-from api.views.messages import messages_bp
 from api.models import db
 
 from api import config
-
-from flask_swagger_ui import get_swaggerui_blueprint
-
-SWAGGER_URL = "/swagger"
-API_URL = "/static/swagger.json"
-
-
-swagger_ui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': 'Access API'
-    }
-)
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -25,8 +10,6 @@ app.config.from_object(config)
 db.init_app(app)
 
 app.register_blueprint(users_bp, url_prefix='/users')
-app.register_blueprint(messages_bp, url_prefix='/messages')
-app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
     app.run(debug=True)
