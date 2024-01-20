0741a846699ef7c1cfe789af1c7c0618ba8495e0
Денис Мирчев <78972321+Kerzoken@users.noreply.github.com>
2024-01-20 04:23:02 +0200

    Revert "Updated User and Message models"
    This reverts commit 4c0255b86a4ec25bc58df7c0ec3456d518cd9a2a.

--- api/models/message.py
+++ api/models/message.py
@@ -7,8 +7,6 @@ class Message(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(80), unique=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-
     reciever_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
--- api/models/user.py
+++ api/models/user.py
@@ -6,10 +6,7 @@ class User(db.Model):
     Represents a user in the system.
     """
     id = db.Column(db.Integer, primary_key=True)
-
     username = db.Column(db.String(80), unique=True)
-    email_address = db.Column(db.String(80), unique=True)
-    bio = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(80), unique=True)
 
     def __repr__(self):
