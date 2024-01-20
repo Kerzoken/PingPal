084b898801729aa678d667005e2ce04137006c23
Денис Мирчев <78972321+Kerzoken@users.noreply.github.com>
2024-01-20 04:23:02 +0200

    Revert "Updated models again and made the necessary fields required in the db"
    This reverts commit d94febc30b158ea97dcbcaf045b4588d32fa81ea.

--- api/models/message.py
+++ api/models/message.py
@@ -6,14 +6,11 @@ class Message(db.Model):
     Represents a message in the system.
     """
     id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(80), unique=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
 
-    message = db.Column(db.String(80), nullable=False)
-    date_created = db.Column(
-        db.DateTime, default=db.func.current_timestamp(), nullable=False)
-
-    reciever_id = db.Column(
-        db.Integer, db.ForeignKey('user.id'), nullable=False)
-    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    reciever_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'<Message {self.message}>'
--- api/models/user.py
+++ api/models/user.py
@@ -7,10 +7,10 @@ class User(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
 
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(80), unique=True, nullable=False)
-    bio = db.Column(db.String(80))
+    username = db.Column(db.String(80), unique=True)
+    email_address = db.Column(db.String(80), unique=True)
+    bio = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80), unique=True)
 
     def __repr__(self):
         return f'<User {self.username}>'
